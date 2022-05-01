9c414fe12d4ab43524e180df19133227909c36a4
Pankaj Kumar <pankajkr06@gmail.com>
2022-05-01 17:01:19 +0530

    Modules

--- Modules/7. ddb.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_dynamodb_table" "state_locks" {
-    name = "iacdevops-prod-tfstate"
-    billing_mode = "PAY_PER_REQUEST"
-    hash_key = "LockID"
-    attribute {
-      name = "LockID"
-      type = "S"
-    }
-}
