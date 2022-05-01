1a04da7e030383f202425b837f6e525526377d64
Pankaj Kumar <pankajkr06@gmail.com>
2022-05-01 19:56:05 +0530

    BuildSpec Edited

---
+++ BackendS3_ddb.tf
@@ -0,0 +1,24 @@
+resource "aws_s3_bucket" "terraform_state" {
+    bucket = "pipelineproject"
+    versioning {
+      enabled = true
+    }
+    server_side_encryption_configuration {
+      rule {
+          apply_server_side_encryption_by_default {
+            sse_algorithm = "AES256"
+          }
+      }
+    }
+  
+}
+
+resource "aws_dynamodb_table" "state_locks" {
+    name = "iacdevops-prod-tfstate"
+    billing_mode = "PAY_PER_REQUEST"
+    hash_key = "LockID"
+    attribute {
+      name = "LockID"
+      type = "S"
+    }
+}
---
+++ terraform.tf
@@ -0,0 +1,36 @@
+# Terraform Block
+terraform {
+  required_version = "~> 1.0" 
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 3.0"
+    }
+    null = {
+      source = "hashicorp/null"
+      version = "~> 3.0"
+    }    
+    random = {
+      source = "hashicorp/random"
+      version = "~> 3.0"
+    }            
+  }
+  # Adding Backend as S3 for Remote State Storage
+  backend "s3" {} 
+}
+
+# Provider Block
+provider "aws" {
+  region  = var.aws_region
+  profile = "Prod"
+}
+/*
+Note-1:  AWS Credentials Profile (profile = "default") configured on your local desktop terminal  
+$HOME/.aws/credentials
+*/
+
+# Create Random Pet Resource
+resource "random_pet" "this" {
+  length = 2
+}
+
